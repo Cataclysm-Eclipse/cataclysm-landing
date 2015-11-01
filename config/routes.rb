a6424192639df2e42ce7d091469ab89e68bd8d80
= <cosgrove.tyler.98@gmail.com>
2015-11-01 14:25:55 -0500

    Add error handling
    Still need to style the error handling but...
    If you want to test errors change line 13 in
    `config/environments/development.db` from
    config.consider_all_requests_local       = true to
    config.consider_all_requests_local       = false

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/404" => "errors#not_found"
+  get "/500" => "errors#internal_server_error"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
